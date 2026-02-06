07e58e0ff8ac3d3abf11b82b1dcf2cd237aaae99
Gwen <gwenael.cercus@supinfo.com>
2026-02-06 11:45:32 +0100

    Refactor Supemon game structure and logic

--- Supemon_Loic.c
+++ Supemon_Loic.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 // Structure des compétences
 struct Competence {
     char name[20];
@@ -56,7 +57,128 @@ struct Player {
     int rare_candies;
 };
 
+
+struct Supemon supemon1 = {
+    "Supmander", 1, 0,
+    10, 10,
+    2, 2,
+    1, 1,
+    1, 1,
+    2, 2,
+    2,
+    {
+                {"Scratch", 3, "NONE", 0},
+                {"Growl",   0, "ATK",  1}
+    }
+};
+
+struct Supemon supemon2 = {
+    "Supasaur", 1, 0,
+    9, 9,
+    1, 1,
+    1, 1,
+    3, 3,
+    2, 2,
+    2,
+    {
+                {"Pound",   2, "NONE", 0},
+                {"Foliage", 0, "EVA",  1}
+    }
+};
+
+struct Supemon supemon3 = {
+    "Supirtle", 1, 0,
+    11, 11,
+    1, 1,
+    2, 2,
+    2, 2,
+    1, 1,
+    2,
+    {
+                {"Pound", 2, "NONE", 0},
+                {"Shell", 0, "DEF",  1}
+    }
+};
+
+struct Supemon supemon4 = {
+    "Supfox", 1, 0,
+    8, 8,
+    2, 2,
+    1, 1,
+    2, 2,
+    3, 3,
+    3,
+    {
+                {"Peck",  2, "NONE", 0},
+                {"Focus", 0, "ACC",  1}
+    }
+};
+
+struct Supemon supemon5 = {
+    "Suprock", 1, 0,
+    12, 12,
+    1, 1,
+    3, 3,
+    1, 1,
+    1, 1,
+    1,
+    {
+                {"Smash", 2, "NONE", 0},
+                {"Guard", 0, "DEF",  1}
+    }
+};
+
+struct Supemon supemon6 = {
+    "Supmind", 1, 0,
+    9, 9,
+    1, 1,
+    1, 1,
+    2, 2,
+    3, 3,
+    2,
+    {
+                {"Pulse",  2, "NONE", 0},
+                {"Focus",  0, "ACC",  1}
+    }
+};
+
+struct Supemon* create_random_enemy(struct Supemon *supemon_list, int count) {
+    int index = rand() % count;
+
+    struct Supemon *enemy = malloc(sizeof(struct Supemon));
+    if (!enemy) return NULL;
+
+    *enemy = supemon_list[index]; // Copie complÃ¨te (moves inclus)
+
+    return enemy;
+}
+
+
+int tentative_toucher(struct Supemon attaquant, struct Supemon cible) {
+
+    float proba = (float)attaquant.accuracy / (attaquant.accuracy + cible.evasion) + 0.1f;
+
+    float jet = (float)rand() / (float)RAND_MAX;
+
+    if (jet <= proba) {
+        return 1;
+    }
+    return 0;
+}
+
+
+
+
 int main() {
+    struct Supemon *all_supemons = malloc(6*  sizeof(struct Supemon));
+
+    all_supemons[0] = supemon1;
+    all_supemons[1] = supemon2;
+    all_supemons[2] = supemon3;
+    all_supemons[3] = supemon4;
+    all_supemons[4] = supemon5;
+    all_supemons[5] = supemon6;
+
 
     struct Player p;
     p.nb_supemons = 0;
@@ -157,96 +279,126 @@ int main() {
         int choice2;
         scanf("%d", &choice2);
 
+        // #####################################
+        // 1 -        INTO THE WILD
+        // #####################################
         if (choice2 == 1) {
-            struct Supemon enemy;
-            int type = rand()%3;
-            if (type == 0) {
-                strcpy(enemy.name, "Savage Supmander");
-                enemy.hp = 10; enemy.max_hp = 10;
-                enemy.attack = 1; enemy.base_attack = 1;
-                enemy.defense = 1; enemy.base_defense = 1;
-                enemy.evasion = 1; enemy.base_evasion = 1;
-                enemy.accuracy = 2; enemy.base_accuracy = 2;
-                enemy.speed = 1;
-                }
-            if (type == 1) {
-                strcpy(enemy.name,"Savage Supasaur");
-                enemy.hp =9; enemy.max_hp = 9;
-                enemy.attack=1; enemy.base_attack =1;
-                enemy.defense = 1; enemy.base_defense = 1;
-                enemy.evasion = 3; enemy.base_evasion = 3;
-                enemy.accuracy = 2; enemy.base_accuracy = 2;
-                enemy.speed = 2;
-            }
-            if (type == 2) {
-                strcpy(enemy.name,"Savage Supirtle");
-                enemy.hp =11; enemy.max_hp = 11;
-                enemy.attack=1; enemy.base_attack =1;
-                enemy.defense = 2; enemy.base_defense = 2;
-                enemy.evasion = 2; enemy.base_evasion = 2;
-                enemy.accuracy = 1; enemy.base_accuracy = 1;
-                enemy.speed = 2;
-            }
-            int combat = 0;
-            printf("You go into the Wild!\n");
-            printf("A savage Supemon appears !\n");
-            while (combat == 0) {
-                printf("+----------------------------+\n"
-                             "|Where do you do ?           |\n"
-                             "|    1 - Attack              |\n"
-                             "|    2 - Change Supemon      |\n"
-                             "|    3 - Use an object       |\n"
-                             "|    4 - Run away            |\n"
-                             "|    5 - Capture the Supemon |\n"
-                             "+----------------------------+\n"
-                             "1, 2, 3, 4  or 5: ");
-                int battle;
-                scanf("%d", &battle);
-                int i = p.selected_supemon;
-                if (battle==1) {
-                    printf("+-----------------------------------+\n"
-                                 "|What attack do you want to launch? |\n"
-                                 "|     1 - %-26s|\n""|     2 - %-26s|\n"
-                                 "+-----------------------------------+\n"
-                                 "1 or 2 :", p.team[i].moves[0].name, p.team[i].moves[1].name);
-                    int attack;
-                    scanf("%d",&attack);
-                    if (attack == 1) {
-                        printf("+-----------------------------------+\n"
-                                     "|You make %-26s|\n",p.team[i].moves[0].name);
-                    }
-                    if (attack == 2) {
-                        printf("+-----------------------------------+\n"
-                                     "|You make %-26s|\n",p.team[i].moves[1].name);
-                    }
-                }
-                if (battle==2) {
-                    printf("+---------------------------------------+\n"
-                                 "|Which Supemon would you like to trade? |\n"
-                                 "|     1 - %-30s|\n"
-                                 "+---------------------------------------+\n",p.team[i].name);
-                }
-                if (battle==3) {
-                    printf("+---------------------------------------+\n");
-                }
-                if (battle==4) {
-                    printf("+----------------------------------------+\n"
-                                 "|Are you sure you want to quit the fight?|\n"
-                                 "      1 - Yes !                          |\n"
-                                 "      2 - No !                           |\n"
-                                 "+----------------------------------------+\n"
-                                 "1 or 2 :");
-                    int leave;
-                    scanf("%d",&leave);
-                    if (leave == 1) {
+            // Initialisation d'un ennemi aléatoire via ta liste
+            int type = rand() % 6;
+            struct Supemon enemy = all_supemons[type];
+            // On lui donne un nom "Savage" pour le différencier
+            char wild_name[50];
+            sprintf(wild_name, "Savage %s", enemy.name);
+            strcpy(enemy.name, wild_name);
 
+            int combat = 1;
+            int i = p.selected_supemon;
+
+            printf("You go into the Wild!\n");
+            printf("A %s appears!\n", enemy.name);
+
+            while (combat == 1) {
+                int i = p.selected_supemon;
+
+                //AFFICHAGE DU RECAP DES STATS
+                printf("  %-20s            %-20s\n", "YOUR SUPEMON", "WILD ENEMY");
+                printf("  %-20s            %-20s\n", p.team[i].name, enemy.name);
+                printf("+--------------------------+    +--------------------------+\n");
+                printf("| HP:  %3d / %-3d          |    | HP:  %3d / %-3d          |\n", p.team[i].hp, p.team[i].max_hp, enemy.hp, enemy.max_hp);
+                printf("| ATK: %2d   DEF: %2d       |    | ATK: %2d   DEF: %2d       |\n", p.team[i].attack, p.team[i].defense, enemy.attack, enemy.defense);
+                printf("| ACC: %2d   EVA: %2d       |    | ACC: %2d   EVA: %2d       |\n", p.team[i].accuracy, p.team[i].evasion, enemy.accuracy, enemy.evasion);
+                printf("| SPD: %2d                  |    | SPD: %2d                  |\n", p.team[i].speed, enemy.speed);
+                printf("+--------------------------+    +--------------------------+\n");
+
+                // TON MENU DE COMBAT
+                printf("\n+----------------------------+\n"
+                       "| What do you do?            |\n"
+                       "|    1 - Attack              |\n"
+                       "|    2 - Change Supemon      |\n"
+                       "|    3 - Use an object       |\n"
+                       "|    4 - Run away            |\n"
+                       "|    5 - Capture             |\n"
+                       "+----------------------------+\n"
+                       "1, 2, 3, 4 or 5: ");
+
+                int battle_choice;
+                scanf("%d", &battle_choice);
+
+                if (battle_choice == 1) {
+                    // MENU SELECTION ATTAQUE
+                    printf("+-----------------------------------+\n"
+                           "| What attack do you want to launch?|\n"
+                           "|      1 - %-25s|\n"
+                           "|      2 - %-25s|\n"
+                           "+-----------------------------------+\n"
+                           "1 or 2 : ", p.team[i].moves[0].name, p.team[i].moves[1].name);
+
+                    int move_idx;
+                    scanf("%d", &move_idx);
+                    move_idx--;
+
+                    printf("+-----------------------------------+\n"
+                           "| You use %-26s|\n", p.team[i].moves[move_idx].name);
+                    if (tentative_toucher(p.team[i], enemy)) {
+                        int degats = p.team[i].moves[move_idx].damage;
+
+                        // Si l'attaque fait des dégâts
+                        if (degats > 0) {
+                            // Formule simple : Dégâts + Ton Attaque - Défense Ennemie
+                            int total_dmg = degats + (p.team[i].attack - enemy.defense);
+                            if (total_dmg < 1) total_dmg = 1; // Minimum 1 dégât
+
+                            enemy.hp -= total_dmg;
+                            printf("| BANG! %s lose %d HP.          |\n", enemy.name, total_dmg);
+                        } else {
+                            // Ici on pourra gérer plus tard les effets de stats (Growl, etc.)
+                            printf("| It has no physical effect...      |\n");
+                        }
+                    } else {
+                        printf("| Missed! The enemy dodged it.      |\n");
                     }
-                    if (leave == 2) {
-                        return 0;
+                    printf("+-----------------------------------+\n");
+
+                    // Vérification si l'ennemi est KO
+                    if (enemy.hp <= 0) {
+                        printf("\nVictory! %s is defeated!\n", enemy.name);
+                        printf("You earned 100 Supcoins.\n");
+                        p.supcoins += 100;
+                        combat = 0;
+                        break; // On sort du combat
+                    }
+
+                    //TOUR DE L'ENNEMI
+                    int enemy_move = rand() % 2;
+
+                    if (tentative_toucher(enemy, p.team[i])) {
+                        int e_degats = enemy.moves[enemy_move].damage;
+                        if (e_degats > 0) {
+                            int total_e_dmg = e_degats + (enemy.attack - p.team[i].defense);
+                            if (total_e_dmg < 1) total_e_dmg = 1;
+
+                            p.team[i].hp -= total_e_dmg;
+                            printf("Ouch! %s loses %d HP!\n", p.team[i].name, total_e_dmg);
+                        } else {
+                            printf("%s uses %s, but nothing happens.\n", enemy.name, enemy.moves[enemy_move].name);
+                        }
+                    } else {
+                        printf("%s missed the attack!\n", enemy.name);
+                    }
+
+                    // Vérification si ton Supemon est KO
+                    if (p.team[i].hp <= 0) {
+                        p.team[i].hp = 0;
+                        printf("\n%s fainted! You must go to the Center.\n", p.team[i].name);
+                        combat = 0;
                     }
                 }
-                if (battle==5) {
-                    printf("+---------------------------------------+\n");
+                else if (battle_choice == 4) {
+                    printf("You fled the battle!\n");
+                    combat = 0;
+                }
+                else {
+                    printf("Not implemented yet, choose Attack or Run!\n");
                 }
             }
         }
@@ -324,7 +476,7 @@ int main() {
                     }
                 }
                 else if (shop_mode == 3) {
-                    shop_running = 0; 
+                    shop_running = 0;
                 }
             }
         }
